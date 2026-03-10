ba25bf258993e95c2b95b7195f5449b2eeea73b9
Samuel <samuel.corchia@gmail.com>
2026-03-10 19:38:59 +0100

    Add instagram on footer - corrections

--- src/resources/views/partials/footer.blade.php
+++ src/resources/views/partials/footer.blade.php
@@ -10,7 +10,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.77,7.46H14.5v-1.9c0-.9.6-1.1,1-1.1h3V.5h-4.33C10.24.5,9.5,3.44,9.5,5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4Z"/></svg>
                     </a>
                     <a href="https://www.instagram.com/lachaume18/" target="_blank" rel="noopener noreferrer" class="instagram-link">
-                        <i class="fab fa-instagram"></i> Suivez-nous sur Instagram
+                        <i class="fab fa-instagram"></i>
                     </a>
                 </div>
             </div>
--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -9,5 +9,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet">
     <!-- CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="/css/style.css">
 </head>
