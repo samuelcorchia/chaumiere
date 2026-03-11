a5826adee77597e2d340806faf3a67006aa849c7
Samuel <samuel.corchia@gmail.com>
2026-03-11 11:54:20 +0100

    add link on admin header to access admin concerts

--- src/resources/views/partials/admin_navbar.blade.php
+++ src/resources/views/partials/admin_navbar.blade.php
@@ -6,9 +6,8 @@
             <a href="{{ route('admin.tables') }}" class="{{ $page == 'tables' ? 'active' : '' }}">Tables</a>
             <a href="{{ route('admin.quotas') }}" class="{{ $page == 'quotas' ? 'active' : '' }}">Quotas</a>
             <a href="{{ route('admin.reservations') }}" class="{{ $page == 'reservations' ? 'active' : '' }}">Réservations</a>
-            
+            <a href="{{ route('admin.concerts') }}" class="{{ $page == 'concerts' ? 'active' : '' }}">Concerts</a>            
             <a href="/" target="_blank">Voir le site</a>
-
             <span style="float: right; margin-left: 2em; width: 2em; filter: grayscale(1) invert(1);"><a href="javascript:void" onclick="document.getElementById('logout-form').submit();"><img src="/images/icons/power.png" alt="Deconnexion" /></a></span>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
