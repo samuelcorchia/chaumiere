0a3232d2f06540e9cf4dc834c9a95104b6f11b4d
Samuel <samuel.corchia@gmail.com>
2026-03-11 10:24:38 +0100

    force le https pour site distant

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+     public function boot()
     {
-        //
+        // Force le HTTPS sur tous les liens si on n'est pas en local pur
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            \URL::forceScheme('https');
+        }
     }
 }
